66e6ea00ba46bedd19aeb4e1704df0613d95758a
Amit Mankotia <amit100.mankotia@gmail.com>
2022-07-27 02:00:43 +0530

    modal

--- Script.js
+++ Script.js
@@ -1,10 +1,14 @@
-// let mybtn=document.querySelector(".btn")
-// mybtn.addEventListener("click",function(){
-// let modal=false
-//     if(!modal){
-        
-//     }
-//     else{
-    
-//     }
-// })
+let addBtn=document.querySelector(".add-btn");
+let modalCont=document.querySelector(".modal-cont")
+// console.log(modalcont)
+// console.log(mybtn);
+var isModalPresent=false;
+addBtn.addEventListener("click",function(){
+if(!isModalPresent){
+modalCont.style.display="flex";
+}
+else if(isModalPresent){
+modalCont.style.display="none";
+}
+isModalPresent=!isModalPresent;
+})
